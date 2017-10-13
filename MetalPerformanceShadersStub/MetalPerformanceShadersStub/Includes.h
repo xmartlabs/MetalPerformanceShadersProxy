f40faae6daa4697c5c3858b9c4ae8dcfd1600bbb
Martin Barreto <martin@xmartlabs.com>
2017-10-13 12:41:18 -0300

    remove example project. It’s not longer needed.

--- MetalPerformanceShadersStub/MetalPerformanceShadersStub/Includes.h
+++ MetalPerformanceShadersStub/MetalPerformanceShadersStub/Includes.h
@@ -6,25 +6,6 @@
 //  Copyright © 2017 Xmartlabs. All rights reserved.
 //
 
-#import "MPSCNN.h"
-#import "MPSImage.h"
-#import "MPSImageConversion.h"
-#import "MPSImageConvolution.h"
-#import "MPSImageHistogram.h"
-#import "MPSImageIntegral.h"
-#import "MPSImageKernel.h"
-#import "MPSImageMedian.h"
-#import "MPSImageMorphology.h"
-#import "MPSImageResampling.h"
-#import "MPSImageThreshold.h"
-#import "MPSImageTranspose.h"
-#import "MPSImageConversion.h"
-#import "MPSKernel.h"
-#import "MPSMatrix.h"
-#import "MPSMatrixMultiplication.h"
-#import "MPSTypes.h"
-
 #import "CVMetalTexture.h"
 #import "CVMetalTextureCache.h"
-
 #import "CAMetalDrawable.h"
